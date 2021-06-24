5960a81feb50284174d682fad99fd1eec418b1d0
Nehemiah Amanuel <nehemiaha1@outlook.com>
2021-06-24 11:15:05 +0300

    Add background style in javascript

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const container = document.querySelector('#container');
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        let grid = document.createElement('div');
-        grid.addEventListener('mouseover', (e) => {
-            e.target.classList.toggle('hover');
-        })
-        container.appendChild(grid);
-    }
+
+for (let i = 0; i < 16 * 16; i++) {
+    let grid = document.createElement('div');
+    grid.addEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = `rgb(0, 0, 0)`;
+    })
+    container.appendChild(grid);    
 }
