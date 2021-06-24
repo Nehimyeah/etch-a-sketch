7ddae01e98e3967add4d741630b16decd6c0a8ab
Nehemiah Amanuel <nehemiaha1@outlook.com>
2021-06-24 10:59:06 +0300

    Add hover event listener to pain grid

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const container = document.querySelector('#container');
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         let grid = document.createElement('div');
+        grid.addEventListener('mouseover', (e) => {
+            e.target.classList.toggle('hover');
+        })
         container.appendChild(grid);
     }
 }
