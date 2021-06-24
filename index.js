02cfd1aaa407e8003810b8df2542e3e497ef1c06
Nehemiah Amanuel <nehemiaha1@outlook.com>
2021-06-24 15:40:55 +0300

    Create the prompt to get the grid size

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const container = document.querySelector('#container');
 
+let row = 16;
 
-for (let i = 0; i < 16 * 16; i++) {
-    let grid = document.createElement('div');
-    grid.addEventListener('mouseover', (e) => {
-        e.target.style.backgroundColor = `rgb(0, 0, 0)`;
-    })
-    container.appendChild(grid);    
+function create(row) {
+    container.style.gridTemplateRows = `repeat(${row}, 1fr)`;
+    container.style.gridTemplateColumns = `repeat(${row}, 1fr)`;
+    container.innerHTML = '';
+    for (let i = 0; i < row * row; i++) {
+        let grid = document.createElement('div');
+        grid.addEventListener('mouseover', (e) => {
+            e.target.style.backgroundColor = `rgb(0, 0, 0)`;
+        })
+        container.appendChild(grid);    
+    }
 }
+
+create(row);
 
 const grids = document.querySelectorAll('#container > div');
 const clear = document.querySelector('#clear');
@@ -16,4 +24,19 @@ clear.addEventListener('click', (e) => {
         grid.style.backgroundColor = 'rgb(255, 255, 255)';
     }
 });
+
+const pt = document.querySelector('#prompt');
+pt.addEventListener('click', (e) => {
+    do {
+        row = prompt('Enter row value less than 100', "integer value");
+    } while (isNaN(row));
+    if (row === null) {
+        row = 16;
+    }
+    row = parseInt(row);
+
+    create(row);
+})
+
+
 
